97e96f5cbef60528cf01492c3a4a199ef67e0a24
Adrian DC <radian.dc@gmail.com>
2016-12-11 11:38:49 +0100

    loire: config: Use unique variables for device paths
     * Avoid hardcoding the paths by using common
        variables declared once for path accesses
    
    Change-Id: I81226a249c6258360c540c80ae4dfe5073439921
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- platform.mk
+++ platform.mk
@@ -12,13 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Platform path
+PLATFORM_COMMON_PATH := device/sony/loire
+
 $(call inherit-product, device/sony/common/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 SOMC_PLATFORM := loire
 
-SONY_ROOT := device/sony/loire/rootdir
+SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -55,7 +58,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/loire/overlay
+    $(PLATFORM_COMMON_PATH)/overlay
 
 # Platform Init
 PRODUCT_PACKAGES += \
