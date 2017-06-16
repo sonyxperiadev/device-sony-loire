ba8ccd6572f32488f59a896497089d1473da22eb
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-16 16:39:18 +0200

    remove the 8Mp camera limit
    on release v11 the camera functionality is improved and there
    is no need to limit the camera resolution
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -141,7 +141,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.tilt_detector=false \
     ro.qti.sensors.dpc=false \
     ro.qti.sensors.wu=true
-
-## 8MP Switch for ES
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.8mp.config=true
