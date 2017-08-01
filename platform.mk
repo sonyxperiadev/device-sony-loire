b5c02466cacb8c0244ed5a6f2ceac8662347488e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-08-01 09:50:50 +0200

    load aanc_tuning_mixer_wcd9335
    the AOSP audio HAL expects aanc_tuning_mixer_wcd9335 file
    to load the aanc_tuning_mixer
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -23,10 +23,13 @@ SOMC_PLATFORM := loire
 
 SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/etc/aanc_tuning_mixer_wcd9335.txt:system/etc/aanc_tuning_mixer_wcd9335.txt \
+    $(SONY_ROOT)/system/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml
+
 # Media
 PRODUCT_COPY_FILES += \
-    $(SONY_ROOT)/system/etc/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(SONY_ROOT)/system/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(SONY_ROOT)/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml
