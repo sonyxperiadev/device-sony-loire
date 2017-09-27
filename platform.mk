0ffad7744cefccd0e3c713d43aba8ac29fd8155e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-27 14:03:39 +0200

    move rild.libpath to common
    the rild.libpath is common to all devices and should not be
    declared in each platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -111,11 +111,6 @@ PRODUCT_PACKAGES += \
     brcm-uim-sysfs \
     libfmjni
 
-# RILD
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM
-
 # OpenGLES Nougat version
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
