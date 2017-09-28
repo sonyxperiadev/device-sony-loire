dd6b9e385953d17572c7ab18e5a46997a68e7944
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-28 15:53:02 +0200

    move NFC permissions and packages to common
    All devices use the same NFC packages and there is no need to
    define them on each device configuartion
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -63,9 +63,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
@@ -75,12 +73,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     fstab.loire \
     init.loire.pwr
-
-# NFC packages
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    NfcNci \
-    Tag
 
 # Audio
 PRODUCT_PACKAGES += \
