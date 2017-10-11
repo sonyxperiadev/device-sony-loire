9be61dacf0f780b3e78451cbc11cce37d2c71d39
Alin Jerpelea <alin.jerpelea@sony.com>
2017-10-11 13:10:51 +0200

    load rild.libpath from /odm partition
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
 
 # RILD
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    rild.libpath=/odm/lib64/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM
 
 # system prop for opengles version
