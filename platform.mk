9019ba66d1312321233265b7d9f034822b49e221
Alin Jerpelea <alin.jerpelea@sony.com>
2017-12-19 11:01:02 +0100

    move audio configuration to vendor
    In Android 8 we can load the audio configuration from the
    vendor partition
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -25,8 +25,8 @@ SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(SONY_ROOT)/system/etc/aanc_tuning_mixer_wcd9335.txt:system/etc/aanc_tuning_mixer_wcd9335.txt \
-    $(SONY_ROOT)/system/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(SONY_ROOT)/vendor/etc/aanc_tuning_mixer_wcd9335.txt:$(TARGET_COPY_OUT_VENDOR)/etc/aanc_tuning_mixer_wcd9335.txt \
+    $(SONY_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
 # Media
 PRODUCT_COPY_FILES += \
