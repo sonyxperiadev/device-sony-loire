464b59c284e77ee912eb58bebd78d0b6b938b696
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-13 18:19:39 +0200

    declare SOMC_KERNEL_VERSION
    the SOMC_KERNEL_VERSION is used by the build system
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -22,6 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 SOMC_PLATFORM := loire
+SOMC_KERNEL_VERSION := 4.4
 
 SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
